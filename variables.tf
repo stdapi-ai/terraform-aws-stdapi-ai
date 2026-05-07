f12e190da19a1ee7d64b83e5bead27b7e461e60a
jgoutin <contact@jgoutin.dev>
2026-05-07 23:21:08 +0200

    chore: Bump default version_to_deploy to 1.11.1

--- variables.tf
+++ variables.tf
@@ -565,7 +565,7 @@ variable "image_generation_model" {
 variable "version_to_deploy" {
   description = "Container image version tag from AWS Marketplace. Leave unset to automatically use the latest stable version. Only override for testing or rollback purposes."
   type        = string
-  default     = "1.11.0"
+  default     = "1.11.1"
 }
 
 # KMS configuration
